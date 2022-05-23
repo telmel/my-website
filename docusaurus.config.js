0b9045c7565727926a6be8a858bd7b59be4f6a52
telmel <freshmdn@gmail.com>
2022-05-24 00:48:40 +0500

    rrereretertreter

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'telmel', // Usually your GitHub org/user name.
   projectName: 'telmel.github.io', // Usually your repo name.
-  deploymentBranch: "gh-pages",
+  deploymentBranch: "master",
 
 
   // Even if you don't use internalization, you can use this field to set useful
